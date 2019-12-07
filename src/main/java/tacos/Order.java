9a3a7b540cfa14228680afd0f640a36a581728ad
lom <diskedge@yandex.ru>
2019-12-07 10:01:11 +0700

    orderForm

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -4,5 +4,12 @@ import lombok.Data;
 
 @Data
 public class Order {
-
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
 }
